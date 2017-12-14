ae9d6339ef20f6d8b5b7a523bebfb92300efdc62
lailaouahi <laila.ouahi@outlook.fr>
2017-12-14 20:05:50 +0000

    je t'aime mon bébé d amour o sber 3lia ila jitk stressée <3

---
+++ app/src/main/java/com/example/user/logregister/ChoixSenario.java
@@ -0,0 +1,13 @@
+package com.example.user.logregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoixSenario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_senario);
+    }
+}
